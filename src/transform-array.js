583355066d4d1ecec4d2be7861c3905efd1834cc
yablochkov <dima5544@yandex.ru>
2021-03-29 04:14:11 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    if(arr === undefined || arr.length === null){
+        return null;}
+    if (!Array.isArray){
+        throw new Error();
+       } 
+    if (arr.length == 0) return [];
+    let newArr = [];
+    for (let item = 0; item < arr.length; item++){
+
+      if(arr[item] == '--double-next'){
+        if (arr[item + 1] != undefined)
+          newArr.push(arr[item + 1]);
+
+      } else if (arr[item] == '--double-prev'){
+        if(arr[item - 2] !== '--discard-next' && arr[item - 1] != undefined){
+        newArr.push(newArr[newArr.length - 1]);
+        }
+       
+     } else if (arr[item] == '--discard-next'){
+       item++;
+     
+    } else if (arr[item] == '--discard-prev'){
+       if(arr[item - 2] !== '--discard-next' && arr[item - 1] !== undefined){
+       newArr.pop();
+       }
+    
+     } else {
+       newArr.push(arr[item]);
+     }
+   }
+    return newArr;
+}
